b075b641e63aff731e75c5445a32ea5f0eb923ae
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2021-11-10 15:49:47 -0500

    added .env for env variables and dotenv package

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+const API_KEY = process.env.WEATHERAPP_API_KEY;
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
 
 // open up server
 app.listen(port, function(req, res){
