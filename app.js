90dcd15c03504b2449c3368b5e1cbf591431beb0
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2021-11-10 17:04:21 -0500

    set some consts for weather values

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const API_KEY = process.env.WEATHERAPP_API_KEY;
 const express = require('express');
 const app = express();
 const port = 3000;
+const https = require('https');
+
+// get data from API
+const units = "imperial";
+const q = "q=London";
+const icon = "";
+const url = "https://api.openweathermap.org/data/2.5/weather?";
 
 
+app.get('/weather', function(req,res){
+   // console.log(req.body);
+    
+})
+app.post('/weather', function(req, res){
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname+"/index.html");
 })
 
 // open up server
